a567367ad61973682fe99654ca96b5adbc95c47c
ragesh <sonuragesh18@gmail.com>
2018-12-12 23:44:17 +0100

    "commit before submission"

--- partitioning.py
+++ partitioning.py
@@ -48,14 +48,14 @@ def random_data():
 	global n     # Number of tasks to be partitioned
 	global tasks # List that stores the instances of tasks
 	tasks = []
-	random.seed()
-	n = random.randrange(2,7)
+	random.seed() #Random seeding
+	n = random.randrange(2,7)  #random in range of 2 and 7
 	print("number of tasks",n)
 
 	for i in range(n):    
 		task_id = i
-		period = random.randrange(9,20)
-		WCET = random.randrange(4,8)
+		period = random.randrange(9,20) #random period
+		WCET = random.randrange(4,8)    #random WCET
 		u =  WCET/period
 		# Limit U by 2 decimal places without rounding off
 		U = truncate(u,2)
@@ -110,17 +110,13 @@ def schedulability():
 	sched_factor = truncate(sched_fac, 2)
 	print("\tsched_factor", sched_factor)
  
-
 def NEXT_FIT():
 	# 'n' tasks and 'm' processors
 	m = 1      				#core counter
 	c = 1                   #initial core count
 	cores = []              #instance list of core class
 	core_rem = sched_factor #Store remaining U factor of core
-	core_buff = []          #Store the utilization of cores
-	merged_cores = []       #Store the non-duplicate cores
-	id_list = []			#Stores id list of cores
-	id_merged = []			#Stores id list of merged cores
+
 	for i in range(n):
 		# if a task cannot fit into same core
 		if(tasks[i].U > core_rem):   #task does not fit into same core
@@ -139,60 +135,49 @@ def NEXT_FIT():
 	# sorting the processors based on core_id
 	cores = sorted(cores, key=lambda cores:cores.core_id )
 	for i in range(len(cores)):
-		id_list.append(cores[i].core_id)
-	# Metrics are calculated here
-	# Inorder to remove the multiple instances of core with same core_id they are merged
-	# Check the next index in the id_list. If an element is not equal to the element in the 
-	# next index, it is the last duplicate
-	id_merged = [i for i, x in enumerate(id_list) if i == len(id_list) - 1 or x != id_list[i + 1]]
-	for i in id_merged:
-		merged_cores.append(cores[i])
-	
+		print("--------------------------------")
+		print("\nCORE  %d"%cores[i].core_id)
+		print("\tcore number       ",cores[i].core_id)
+		print("\tcore load capacity",cores[i].core_U)
+		print("\tcore rem capacity ",cores[i].core_rem_U)
+		print("\tTask in core      ",cores[i].task_ID) 
+		print("\tTask Utilization  ",cores[i].task_U) 
+		print("--------------------------------")
 	print("\n\tNumber of processors used for NEXT FIT",m)
-	for i in range(len(merged_cores)):
-		print("\n------------------------")
-		print("\nCORE  %d"%merged_cores[i].core_id)
-		print("\tcore number       ",merged_cores[i].core_id)
-		print("\tcore total load   ",merged_cores[i].core_U - merged_cores[i].core_rem_U)
-		print("\tcore remaing U    ",merged_cores[i].core_rem_U)
-		print("------------------------")
-
-	# Display of main metrics
-	for i in range(len(merged_cores)): #truncate the utilization value
-		core_buff.append(truncate(merged_cores[i].core_U - merged_cores[i].core_rem_U,2)) 
-	core_buff.sort() #sorting the U list for finding max and min values
-	print("Utilization factor of cores		  ", core_buff)
-	print("Maximum Utilization factor of cores", core_buff[-1])
-	print("Minimum Utilization factor of cores", core_buff[0])
+	dispaly_metrics(cores)
 
 def FIRST_FIT():
-	# 'n' tasks and 'm' processors
+	# 'n' tasks and 'res' processors
 	global need
 	res = 0
-	bin_rem = [0]*(n*2)			# array to store remaining space in cores
+	core_remain = [0]*n			# array to store remaining space in cores
 	cores = []  			#instance list of core class
 	core_rem = sched_factor #Store remaining U factor of core
-	core_buff = []          #Store the utilization of cores
-	merged_cores = []       #Store the non-duplicate cores
-	id_list = []			#Stores id list of cores
-	id_merged = []			#Stores id list of merged cores 
 	
 	for i in range(n):
 		need = 0
 		# Find the first core that can schedule the task
 		for j in range(res):
-			if bin_rem[j] >= tasks[i].U:
-				bin_rem[j] = bin_rem[j] - tasks[i].U
-				cores.append(core(res, sched_factor, bin_rem[j],i,tasks[i].U)) 
+			if core_remain[j] >= tasks[i].U:
+				core_remain[j] = tasks[i].U - core_remain[j] 
+				cores.append(core(res,
+							sched_factor,
+							truncate(tasks[i].U - core_remain[j] ,2),
+							i,
+							tasks[i].U)) 
 				break
 			else:
 				need = need +1
 		
-		# If no core can schedule the task
+		# If no core can schedule the task add new core
 		if need == res:
-			bin_rem[res] = sched_factor - tasks[i].U
-			res = res +1   #task i is added to a new 
-			cores.append(core(res, sched_factor, bin_rem[res] ,i,tasks[i].U)) 
+			core_remain[res] = sched_factor - tasks[i].U
+			res = res +1   #task i is added to a new core
+			cores.append(core(res,
+							sched_factor, 
+							truncate(sched_factor - tasks[i].U,2),
+							i,
+							tasks[i].U)) 
 	
 	for i in range(len(cores)):
 		print("--------------------------------")
@@ -204,94 +189,92 @@ def FIRST_FIT():
 		print("\tTask Utilization  ",cores[i].task_U) 
 		print("--------------------------------")
 	print("\n\tNumber of processors used for FIRST FIT",res)
-	# sorting the processors based on core_id
-	cores = sorted(cores, key=lambda cores:cores.core_id )	
-	for i in range(len(cores)):
-		id_list.append(cores[i].core_id)
-
-
-	# Metrics are calculated here
-	# Inorder to remove the multiple instances of core with same core_id they are merged
-	id_merged = [i for i, x in enumerate(id_list) if i == len(id_list) - 1 or x != id_list[i + 1]]
-	for i in id_merged:
-		merged_cores.append(cores[i])
-
-	print("\n\tNumber of processors used for FIRST FIT",res)
-	# Display of main metrics
-	for i in range(len(merged_cores)): #truncate the utilization value
-		core_buff.append(truncate(merged_cores[i].core_U - merged_cores[i].core_rem_U,2)) 
-	core_buff.sort() #sorting the U list for finding max and min values
-	print("Utilization factor of cores		  ", core_buff)
-	print("Maximum Utilization factor of cores", core_buff[-1])
-	print("Minimum Utilization factor of cores", core_buff[0])
+	dispaly_metrics(cores)
 
 def BEST_FIT():
 	# 'n' tasks and 'm' processors
-	global Need
 	res = 0
-	bin_rem = [0]*(n*2)			# array to store remaining space in cores
+	core_remain = [0]*n	# array to store remaining space in cores
 	cores = []  			#instance list of core class
 	core_rem = sched_factor #Store remaining U factor of core
+	count = sched_factor    
+
+	for i in range(n):
+		min = count +1
+		for j in range(res):
+			if (core_remain[j] >= tasks[i].U 
+				and core_remain[j]-tasks[i].U < min):
+				min = core_remain[j] - tasks[i].U
+				c = j
+				break
+		
+		if min == count+1:
+			core_remain[res] = sched_factor - tasks[i].U
+			res = res  + 1
+			cores.append(core(res,
+						sched_factor,
+						truncate(sched_factor - tasks[i].U,2),
+						i,
+						tasks[i].U))
+		else:
+			core_remain[c] = tasks[i].U - core_remain[c]
+			cores.append(core(res,
+						sched_factor,
+						truncate(tasks[i].U - core_remain[c],2),
+						i,
+						tasks[i].U))
+	
+	for i in range(len(cores)):
+		print("--------------------------------")
+		print("\nCORE  %d"%cores[i].core_id)
+		print("\tcore number       ",cores[i].core_id)
+		print("\tcore load capacity",cores[i].core_U)
+		print("\tcore rem capacity ",cores[i].core_rem_U)
+		print("\tTask in core      ",cores[i].task_ID) 
+		print("\tTask Utilization  ",cores[i].task_U) 
+		print("--------------------------------")
+	print("\n\tNumber of processors used for BEST FIT",res)
+	dispaly_metrics(cores)
+
+def dispaly_metrics(cores):
 	core_buff = []          #Store the utilization of cores
 	merged_cores = []       #Store the non-duplicate cores
 	id_list = []			#Stores id list of cores
 	id_merged = []			#Stores id list of merged cores 
 	
-	for i in range(n):
-		min = count +1
-		# Find the first core that can schedule the task
-		for j in range(res):
-			if bin_rem[j] >= tasks[i].U:
-				bin_rem[j] = bin_rem[j] - tasks[i].U
-				cores.append(core(res, sched_factor, bin_rem[j],i,tasks[i].U)) 
-				break
-			else:
-				need = need +1
-		
-		# If no core can schedule the task
-		if need == res:
-			bin_rem[res] = sched_factor - tasks[i].U
-			res = res +1   #task i is added to a new 
-			cores.append(core(res, sched_factor, bin_rem[res] ,i,tasks[i].U)) 
+	# The cores are sorted based on the core_id
+	cores = sorted(cores, key=lambda cores:cores.core_id )	
+	for i in range(len(cores)):
+		id_list.append(cores[i].core_id)
+	# Metrics are calculated here
 	
-	# for i in range(len(cores)):
-	# 	print("--------------------------------")
-	# 	print("\nCORE  %d"%cores[i].core_id)
-	# 	print("\tcore number       ",cores[i].core_id)
-	# 	print("\tcore load capacity",cores[i].core_U)
-	# 	print("\tcore rem capacity ",cores[i].core_rem_U)
-	# 	print("\tTask in core      ",cores[i].task_ID) 
-	# 	print("\tTask Utilization  ",cores[i].task_U) 
-	# 	print("--------------------------------")
-	print("\n\tNumber of processors used for FIRST FIT",res)
-	# sorting the processors based on core_id
-	# cores = sorted(cores, key=lambda cores:cores.core_id )	
-	# for i in range(len(cores)):
-	# 	id_list.append(cores[i].core_id)
-
-		
-	# # Metrics are calculated here
-	# # Inorder to remove the multiple instances of core with same core_id they are merged
-	# id_merged = [i for i, x in enumerate(id_list) if i == len(id_list) - 1 or x != id_list[i + 1]]
-	# for i in id_merged:
-	# 	merged_cores.append(cores[i])
-
-	# print("\n\tNumber of processors used for FIRST FIT",res)
-	# # Display of main metrics
-	# for i in range(len(merged_cores)): #truncate the utilization value
-	# 	core_buff.append(truncate(merged_cores[i].core_U - merged_cores[i].core_rem_U,2)) 
-	# core_buff.sort() #sorting the U list for finding max and min values
-	# print("Utilization factor of cores		  ", core_buff)
-	# print("Maximum Utilization factor of cores", core_buff[-1])
-	# print("Minimum Utilization factor of cores", core_buff[0])
+	# Inorder to remove the multiple instances of core
+	#  with same core_id they are merged
+	id_merged = [i for i, x in enumerate(id_list) 
+				if i == len(id_list) - 1 or x != id_list[i + 1]]
+	# A merged_core array is created to store the core_id
+	for i in id_merged:
+		merged_cores.append(cores[i])
 	
+	# Display of main metrics
+	for i in range(len(merged_cores)): #truncate the utilization value
+		core_buff.append(truncate(merged_cores[i].core_U 
+							- merged_cores[i].core_rem_U,2)) 
 
-# Driver program
+	core_buff.sort() #sorting the U list for finding max and min values
+	print("Utilization factor of cores		  ", core_buff)
+	print("Maximum Utilization factor of cores", core_buff[-1])
+	print("Minimum Utilization factor of cores", core_buff[0])
+
 if __name__ == '__main__':
-	random_data()
-	# read_data()
-	# hp = hyperperiod()
-	schedulability()
-	# NEXT_FIT()
-	FIRST_FIT()
-	# BEST_FIT()
+	random_data()		#Random task set generation
+	# read_data()			#Reads taskset from user
+	# hp = hyperperiod()	#Calculates hyperperiod	
+	schedulability()	#Check for feasibility
+	print("\n\n\n\n")	
+	NEXT_FIT()			#Next fit partitioning
+	print("\n\n\n\n")
+	FIRST_FIT()			#First fit partitioning
+	print("\n\n\n\n")
+	BEST_FIT()			#Best fit partitioning
+	print("\n\n\n\n")
